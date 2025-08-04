fc12ad1f659f19b11bf52b82b6778bc9f1516912
Akhil <bishtakhil04@gmail.com>
2025-08-04 19:23:15 +0530

    Day 2: User API CRUD done

---
+++ day02-user-api/src/main/java/com/example/userApi/Controller/UserController.java
@@ -0,0 +1,69 @@
+package com.example.userApi.Controller;
+
+import com.example.userApi.Model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    ArrayList<User> users = new ArrayList<>();
+
+    @GetMapping("/display")
+    public List<User> show(){
+        return users;
+    }
+
+    @GetMapping("/display/{id}")
+    public User getuserbyid(@PathVariable int id){
+        for(User u: users){
+            if(u.getId()==id){
+                return u;
+            }
+        }
+        return null;
+    }
+
+    //post
+    @PostMapping("/add")
+    public void add(@RequestBody User user){
+        users.add(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public void update(@PathVariable int id ,@RequestBody User user){
+        for(User u: users){
+            if(u.getId()==id){
+                if(user.getId() !=0 ) {
+                    u.setId(user.getId());
+                }
+                if(user.getName() != null){
+                    u.setName(user.getName());
+                }
+                if(user.getEmail() != null){
+                    u.setEmail(user.getEmail());
+                }
+
+
+
+            }
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id){
+        boolean removed = users.removeIf(u -> u.getId() == id);
+        if (removed) {
+            return ResponseEntity.ok("User deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
+
+
+}
---
+++ day02-user-api/src/main/java/com/example/userApi/UserApi.java
@@ -0,0 +1,13 @@
+package com.example.userApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApi.class, args);
+	}
+
+}
